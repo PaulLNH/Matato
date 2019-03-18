334c5348c6459a5c4a946f5c5610045c2aeae069
plaird730 <plaird730@gmail.com>
2019-03-17 21:44:36 -0400

    Added experience and education components, routes and actions. Tested add experience and add education, both post to DB with no errors, error handling is operational on both.

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,7 @@ import {
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
-  SET_CURRENT_USER,
+  SET_CURRENT_USER
 } from "./types";
 
 // Get current profile
@@ -53,22 +53,48 @@ export const clearCurrentProfile = () => {
   };
 };
 
-// Delete Account & Profile
-export const deleteAccount = () => dispatch => {
-  if(window.confirm('Are you sure? This can NOT be undone!')) {
-    axios
-    .delete('/api/profile')
-    .then(res =>
-      dispatch({
-        type: SET_CURRENT_USER,
-        payload: {},
-      })
-    )
+// Add experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post("/api/profile/experience", expData)
+    .then(res => history.push("/dashboard"))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
       })
     );
+};
+
+// Add education
+export const addEducation = (eduData, history) => dispatch => {
+  axios
+    .post("/api/profile/education", eduData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete Account & Profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
+    axios
+      .delete("/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
   }
-}
+};
