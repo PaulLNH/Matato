c3feb28af55fc8eea2eda5ca10c12a2e7b1864b1
plaird730 <plaird730@gmail.com>
2019-03-22 13:51:30 -0400

    built scripts for deploy

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // Require dependencies
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = requir("path");
 
 // API route logic
-const user = require('./routes/api/user');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const user = require("./routes/api/user");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 // Sets a port for our app to listen on
 // either PORT set by enviornmental variables
@@ -21,26 +22,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log('MongoDB Connected'))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error(err));
 
 // Passport middleware
 app.use(passport.initialize());
 
-// Pass the 'passport' node_module as 
+// Pass the 'passport' node_module as
 // an arg into the passport config file
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Use the following API endpoints
 // passing in the route config
-app.use('/api/users', user);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", user);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static foolder
+  app.use(express.static("client/build"));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Start our server and listen on the designated PORT
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
